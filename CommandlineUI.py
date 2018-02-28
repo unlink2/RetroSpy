f9077a01e7fc6692d49ec190720ee80f5aacc03f
Lukas Myers <lukaskrickl@gmail.com>
2018-02-28 16:42:09 +0100

    The debug window now is a command line dump instead

---
+++ CommandlineUI.py
@@ -0,0 +1,32 @@
+# This tk winodw is used to log the state of pins
+
+import time
+from InputSource import InputSource
+from ControllerState import ControllerState
+
+# This is the experimental mega drive input view window.
+# This will move to the normal view window once it is implemented.
+
+
+class CommandlineUI:
+    def __init__(self, comport, ui=0.05, device_type = 'nes'):
+        self.inputsource = InputSource.makeInputSource(device_type)
+        self.inputsource.makeControllerReader(comport=comport)
+        self.inputsource.controllerreader.controllerstate += self.on_state
+
+        self.last_state = ControllerState({}, {})
+
+        self.ui = ui
+        self.running = True
+
+        while self.running:
+            self.update()
+
+    def update(self):
+        self.inputsource.controllerreader.update()
+        print(self.last_state.buttons)
+        print(self.last_state.analogs)
+        time.sleep(self.ui)
+
+    def on_state(self, sender, newstate, *args, **kwargs):
+        self.last_state = newstate
