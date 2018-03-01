430db343bee79cc1c813a3f14e8260467b31f7f7
Lukas Myers <lukaskrickl@gmail.com>
2018-03-01 18:30:53 +0100

    Added about window and cli command

---
+++ AboutWindow.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from util import ABOUT_TEXT
+
+class AboutWindow():
+    def __init__(self, root):
+        self.root = root
+        self.window = tk.Toplevel(self.root)
+        self.window.title('About')
+
+        abouttext = tk.Text(self.window, height=20, width=81)
+        scrollbar = tk.Scrollbar(self.window)
+        abouttext.pack()
+        scrollbar.config(command=abouttext.yview)
+        abouttext.config(yscrollcommand=scrollbar.set)
+        abouttext.insert(tk.END, ABOUT_TEXT)
